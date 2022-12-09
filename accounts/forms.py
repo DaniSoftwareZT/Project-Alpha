b329ccc68ae0fd06bd75f000f8556260792fbdf4
Daniel <rippedchili@gmail.com>
2022-12-09 13:44:57 -0400

    Feature 10 Complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,3 +8,14 @@ class LoginForm(forms.Form):
         widget=forms.PasswordInput()
     )
 
+
+class SignupForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput()
+    )
+    password_confirmation = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput()
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignupForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
@@ -33,3 +33,26 @@ def login_view(request):
 def user_logout(request):
     logout(request)
     return redirect("login")
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            confirm = form.cleaned_data['password_confirmation']
+
+            if password == confirm:
+                user = User.objects.create_user(username, password)
+                login(request, user)
+                return redirect("list_projects")
+            else:
+                form.add_error(None, "the passwords do not match")
+
+    else:
+        form = SignupForm()
+    context = {
+        "form": form
+    }
+    return render(request, "registration/signup.html", context)
